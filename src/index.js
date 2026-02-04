0ba6cb10a0033b88063570b9dc1b2f3153a6d0b6
Faisal Naseer <fnaseer4@gmail.com>
2026-02-04 14:55:23 +0500

    Update app.listen to bind to all network interfaces

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(PORT, () =>
+app.listen(PORT,'0.0.0.0', () =>
   console.log(`Example app listening on port ${PORT}!`),
 );
